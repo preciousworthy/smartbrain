7601585e26b47b4a9e6d0ca64082cf2133577073
Precious Worthy <preciousworthy@gmail.com>
2026-01-28 21:48:44 -0800

    update fetch urls, signin and register

--- src/Components/Register/register.jsx
+++ src/Components/Register/register.jsx
@@ -23,7 +23,7 @@ class Register extends Component {
   };
 
   onSubmitRegister = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://smartbrain-server-t1zx.onrender.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
